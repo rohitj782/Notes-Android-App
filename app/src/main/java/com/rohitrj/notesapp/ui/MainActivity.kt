4599bd16b9b05b913fb885f1a9dfbd8ab860d82c
Rohit Joshi <44168655+rohitj782@users.noreply.github.com>
2019-06-26 20:52:11 +0530

    initial commit

--- app/src/main/java/com/rohitrj/notesapp/ui/MainActivity.kt
+++ app/src/main/java/com/rohitrj/notesapp/ui/MainActivity.kt
@@ -1,12 +1,30 @@
-package com.rohitrj.notesapp
+package com.rohitrj.notesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.rohitrj.notesapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(materialToolbar)
+
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+
+        //attaching the toolbar with nav controller
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    //handle back track of action bar.
+        override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
     }
 }
